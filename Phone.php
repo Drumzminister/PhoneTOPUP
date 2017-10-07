bf578cf683327c413454054c95686235792357a2
Daniella Mato <dmpire2@gmail.com>
2017-10-07 12:58:07 +0100

    Data top up functionality

--- Phone.php
+++ Phone.php
@@ -14,7 +14,6 @@
             <h5>Phone Top up</h5>
             <input type="search" name="search" id="search" placeholder="Search">
         </div>
-
         <div class="content_two">
             <div class="balance">
                 <h5>Balance</h5>
@@ -23,7 +22,6 @@
                         <strong>
                             <?php
                                   $curl = curl_init();
-
                                   curl_setopt_array($curl, array(
                                     CURLOPT_URL => "https://mobileairtimeng.com/httpapi/balance.php?userid=%2008189115870&pass=dbcc49ee2fba9f150c5e82",
                                     CURLOPT_RETURNTRANSFER => true,
@@ -37,12 +35,9 @@
                                       "postman-token: 28c061c4-a48c-629f-3aa2-3e4cad0641ff"
                                     ),
                                   ));
-
                                   $response = curl_exec($curl);
                                   $err = curl_error($curl);
-
                                   curl_close($curl);
-
                                   if ($err) {
                                     echo "cURL Error #:" . $err;
                                   } else {
@@ -52,7 +47,6 @@
                             </strong></p>
                 </span>
             </div>
-
             <div class="wallet">
                     <h5>Refill Topup Wallet</h5>
                     <p>Select Amount</p>
@@ -76,12 +70,10 @@
                         <img src="images/call.png" alt="call icon"><br><br><hr>
                         <p>Chef's Phone - <strong>417-873-60000</strong> </p>
                         <button class="history">Transaction History</button>
-
                             <div class="selectamount">
                                 <span class="selecttopup"><h6>Select Amount</h6>
                                     <img src="images/naira.png" alt="naira" width="25px" height="25px" >
                                 </span>
-
                                 <span class="reach">
                                     <form action="script.php" method="POST">
                                             <input type="submit" name="load500" id="load500"  value="500"/>
@@ -89,9 +81,8 @@
                                             <input type="submit" name="load2000" id="load2000" value="2000"/>
                                             <input type="submit" name="load3000" id="load3000" value="3000"/>
                                             <input type="submit" name="load4000" id="load4000" value="4000"/>
-                                            <input type="submit" name="load5000" id="load5000" value="5000"/>                                            
+                                            <input type="submit" name="load5000" id="load5000 " value="5000"/>                                            
                                     </form>
-
                                 </span>
                                
                             </div>
@@ -117,13 +108,16 @@
                             <li> <button><img src="images/4.png" width="40" height="40" alt="glo"> </button></li> <hr>
                         </ul>
                         <span class="data">
-                           <button>1.5Gb <br> 30days<br> <strong>N1,000</strong></button>
-                           <button>1.5Gb <br> 30days<br> <strong>N2,000</strong> </button>
-                           <button>1.5Gb <br> 30days<br> <strong>N3,000</strong> </button>
-                           <br>
-                          <button>1.5Gb <br> 30days<br> <strong>N4,000</strong> </button>
-                           <button>1.5Gb <br> 30days<br> <strong>N5,000</strong> </button>
-                           <button>1.5Gb <br> 30days<br> <strong>N6,000</strong> </button>
+                            <form action="datascript.php" method="POST">
+                               <button type="submit" name="data1000" id="data1000"  value="1000">1.5Gb <br> 30days<br> <strong>N1,000</strong></button>
+                               <button type="submit" name="data2000" id="data2000"  value="2000">1.5Gb <br> 30days<br> <strong>N2,000</strong> </button>
+                               <button "type="submit" name="data3000" id="data3000"  value="3000">1.5Gb <br> 30days<br> <strong>N3,000</strong> </button>
+                               <br>
+                               <button type="submit" name="data4000" id="data4000"  value="4000">1.5Gb <br> 30days<br> <strong>N4,000</strong> </button>
+                               <button type="submit" name="data5000" id="data5000"  value="5000">1.5Gb <br> 30days<br> <strong>N5,000</strong> </button>
+                               <button type="submit" name="data6000" id="data6000"  value="6000">1.5Gb <br> 30days<br> <strong>N6,000</strong> </button>
+                            </form>
+                           
                         </span>
                         
                     </div>
